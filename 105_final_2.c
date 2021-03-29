51192def0ee04b49258dbaf25ecdf4c86f722e33
supanat <supanat.ap@gmail.com>
2021-03-29 17:09:22 +0700

    Update 105_final_2.c

--- 105_final_2.c
+++ 105_final_2.c
@@ -1,7 +1,21 @@
 #include<stdio.h>
 int main()
 {
+    int n,a,b,i,j;
 
+    scanf("%d",&n);
+
+    int A[n];
+
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&A[i]);
+    }
+
+    for(i=0;i<n;i++)
+    {
+
+    }
 
 
 
