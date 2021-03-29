242b3903a11cb46db74e160bbb7acba43546ddb2
supanat <supanat.ap@gmail.com>
2021-03-29 18:32:08 +0700

    a

--- 105_final_3.c
+++ 105_final_3.c
@@ -1,39 +1,62 @@
 #include<stdio.h>
 int main()
 {
-    int n1,n2,n3,a,i,j;
+    int n[3],a,i,j,dia[1000]= {0},row[1000]= {0},colum[1000]= {0},k;
 
-    scanf("%d %d %d",&n1,&n2,&n3);
+    for(i=0;i<3;i++)
+    scanf("%d",&n[i]);
 
-    int A[n1][n1],B[n2][n2],C[n3][n3],sum[1000]={0},row[1000]={0},colum[1000]={0};
+    int A[n[0]][n[0]],B[n[1]][n[1]],C[n[2]][n[2]];
 
-    for(i=0;i<n1;i++)
-     {
-         for(j=0;j<n1;j++)
+    for(k=0; k<3; k++)
+    {
+        for(i=0; i<n[k]; i++)
+        {
+            for(j=0; j<n[k]; j++)
             {
-                scanf("%d",&A[i][j]);
+                if(k==0)
+                     scanf("%d",&A[i][j]);
+                else if(k==1)
+                     scanf("%d",&B[i][j]);
+                else if(k==2)
+                     scanf("%d",&C[i][j]);
             }
-     }
-     for(i=0;i<n1;i++)
+        }
+        printf("\n");
+    }
+    for(i=0; i<n[0]; i++)
+    {
+        for(j=0; j<n[0]; j++)
+        {
+            row[i]=A[i][j]+row[i];
+            colum[i]=A[j][i]+colum[i];
+
+        }
+
+    }
+
+    for(i=0; i<n[0]-1; i++)
+    {
+        if(row[i]!=row[i+1])
+        {
+            printf("yes");
+            break;
+        }
+
+        if(colum[i]!=colum[i+1])
+        {
+            printf("yes");
+            break;
+        }
+
+    }
+
+
+
+    /*for(i=0;i<n1;i++)
      {
-         for(j=0;j<n1;j++)
-            {
-
-                row[i]=A[i][j]+row[i];
-                colum[i]=A[j][i]+colum[i];
-            }
-     }
-
-    for(i=0;i<n1;i++)
-     {
-
-
                   printf("%d,%d\n",row[i],colum[i]);
-
-
-
-
-     }
+     }*/
 
 
 
