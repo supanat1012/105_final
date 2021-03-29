49d931b5416d0b449e777294241ef2b8bc69f02e
supanat <supanat.ap@gmail.com>
2021-03-29 18:13:54 +0700

    a

--- 105_final_3.c
+++ 105_final_3.c
@@ -5,13 +5,20 @@ int main()
 
     scanf("%d %d %d",&n1,&n2,&n3);
 
-    int A[n1][n1],B[n2][n2],C[n3][n3],sum[1000]={0},row[1000],colum[1000];
+    int A[n1][n1],B[n2][n2],C[n3][n3],sum[1000]={0},row[1000]={0},colum[1000]={0};
 
     for(i=0;i<n1;i++)
      {
          for(j=0;j<n1;j++)
             {
                 scanf("%d",&A[i][j]);
+            }
+     }
+     for(i=0;i<n1;i++)
+     {
+         for(j=0;j<n1;j++)
+            {
+
                 row[i]=A[i][j]+row[i];
                 colum[i]=A[j][i]+colum[i];
             }
