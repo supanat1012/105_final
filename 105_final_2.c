7548519bfa8f20bbd4e8b3ef84c00a1e7e01433b
supanat <supanat.ap@gmail.com>
2021-03-29 17:28:24 +0700

    Update 105_final_2.c

--- 105_final_2.c
+++ 105_final_2.c
@@ -4,31 +4,33 @@ int main()
     int n,a,b=0,i,max=0;
 
     scanf("%d",&n);
-
-    int A[n];
-
-    for(i=0;i<n;i++)
+    if(n>=2&&n<=1000)
     {
-        scanf("%d",&A[i]);
-    }
+        int A[n];
 
-    for(i=0;i<n;i++)
-    {
-        if(A[i]>max)
+        for(i=0; i<n; i++)
+        {
+            scanf("%d",&A[i]);
+        }
+
+        for(i=0; i<n; i++)
+        {
+            if(A[i]>max)
             {
                 a=i+1;
                 max=A[i];
             }
-        else if(A[i]==max)
+            else if(A[i]==max)
             {
                 b=1;
             }
 
+        }
+        if(b==0)
+            printf("%d",a);
+        else
+            printf("%d %d",a,max);
     }
-    if(b==0)
-    printf("%d",a);
-    else
-    printf("%d %d",a,max);
 
 
 
